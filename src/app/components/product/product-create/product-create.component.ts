ebcf38fdb36d707a70d8d51388418d96116ef710
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-06-27 10:27:41 -0300

    Bindings

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-product-create',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
+  propLegal = 'qualquer';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  fazerAlgo(): void {
+    console.log('Fazendo algo!');
   }
 
 }
